51f761fd472e7b9f43f7f7e025e468985614c57a
figueroavictor <figueroavictor333@gmail.com>
2025-09-23 21:21:45 -0300

    a

--- Proyecto1/myapp/views.py
+++ Proyecto1/myapp/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Estudiante, Profesor, Curso, Entregable
 
 # Create your views here.
 def index(request):
     context = {"mensaje": "Bienvenidos a mi aplicación Django"}
     return render(request, "myapp/index.html", context)
-
-from django.shortcuts import render, get_object_or_404
-from .models import Estudiante, Profesor, Curso, Entregable
 
 
 def lista_estudiantes(request):
